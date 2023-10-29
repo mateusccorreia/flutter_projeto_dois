607249bc6a45d6eda41c9376cb4dc725a6f04241
Mateus Correia <mateuscorreia.dev@gmail.com>
2023-10-29 18:19:19 -0300

    added the files of the API Service and the home page

---
+++ lib/services/api_service.dart
@@ -0,0 +1,20 @@
+import 'dart:developer';
+
+import 'package:http/http.dart' as http;
+import 'package:flutter_projeto_dois/constants.dart';
+import 'package:flutter_projeto_dois/model/user_model.dart';
+
+class ApiService {
+  Future<List<UserModel>?> getUsers() async {
+    try {
+      var url = Uri.parse(ApiConstants.baseUrl + ApiConstants.usersEndPoint);
+      var response = await http.get(url);
+      if (response.statusCode == 200) {
+        List<UserModel> _model = userModelFromJson(response.body);
+        return _model;
+      }
+    } catch (e) {
+      log(e.toString());
+    }
+  }
+}
