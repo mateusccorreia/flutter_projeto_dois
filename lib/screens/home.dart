e30a977446756d8475c183fcd8dbda3fd58ffba8
Mateus Correia <mateuscorreia.dev@gmail.com>
2023-10-30 05:42:52 -0300

    added the Material App in the home page to create a new style

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -11,6 +11,7 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   late List<UserModel>? _userModel = [];
+
   @override
   void initState() {
     super.initState();
@@ -24,41 +25,47 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('REST API - Mateus'),
+    return MaterialApp(
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: Colors.blueGrey
       ),
-      body: _userModel == null || _userModel!.isEmpty
-          ? const Center(
-        child: CircularProgressIndicator(),
-      )
-          : ListView.builder(
-        itemCount: _userModel!.length,
-        itemBuilder: (context, index) {
-          return Card(
-            child: Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Text(_userModel![index].id.toString()),
-                    Text(_userModel![index].username),
-                  ],
-                ),
-                const SizedBox(
-                  height: 20.0,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Text(_userModel![index].email),
-                    Text(_userModel![index].website),
-                  ],
-                ),
-              ],
-            ),
-          );
-        },
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('REST API - Mateus'),
+        ),
+        body: _userModel == null || _userModel!.isEmpty
+            ? const Center(
+          child: CircularProgressIndicator(),
+        )
+            : ListView.builder(
+          itemCount: _userModel!.length,
+          itemBuilder: (context, index) {
+            return Card(
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Text(_userModel![index].id.toString()),
+                      Text(_userModel![index].name),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 20.0,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Text(_userModel![index].email),
+                      Text(_userModel![index].website),
+                    ],
+                  ),
+                ],
+              ),
+            );
+          },
+        ),
       ),
     );
   }
