74ee26c1c9c465f0eb04164c250751865197b327
Mateus Correia <mateuscorreia.dev@gmail.com>
2023-10-31 06:01:04 -0300

    added some styles in the container and padding of the card

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -34,6 +34,7 @@ class _HomeState extends State<Home> {
         appBar: AppBar(
           title: const Text('REST API - Mateus'),
         ),
+        backgroundColor: Colors.purple,
         body: _userModel == null || _userModel!.isEmpty
             ? const Center(
           child: CircularProgressIndicator(),
@@ -42,26 +43,41 @@ class _HomeState extends State<Home> {
           itemCount: _userModel!.length,
           itemBuilder: (context, index) {
             return Card(
-              child: Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Text(_userModel![index].id.toString()),
-                      Text(_userModel![index].name),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 20.0,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Text(_userModel![index].email),
-                      Text(_userModel![index].website),
-                    ],
-                  ),
-                ],
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8.0),
+                side: const BorderSide(color: Colors.white, width: 2.0),
+              ),
+              child: Container(
+                color: Colors.blue,
+                  child: Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: DecoratedBox(
+                        decoration: const BoxDecoration(
+                          color: Colors.blue,
+                        ),
+                        child: Column(
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                Text(_userModel![index].id.toString()),
+                                Text(_userModel![index].name),
+                              ],
+                            ),
+                            const SizedBox(
+                              height: 20.0,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                Text(_userModel![index].email),
+                                Text(_userModel![index].website),
+                              ],
+                            ),
+                          ],
+                        ),
+                      )
+                  )
               ),
             );
           },
