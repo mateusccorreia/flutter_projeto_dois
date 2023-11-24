4b7e14ee94b73674381717370cab10b5ccf2500f
Mateus Correia <mateuscorreia.dev@gmail.com>
2023-11-23 22:05:39 -0300

    refactored the store class

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -16,9 +16,6 @@ class _HomeState extends State<Home> {
     super.initState();
 
     store.getData();
-    store.addListener(() {
-      setState(() {});
-    });
   }
 
   @override
@@ -33,79 +30,80 @@ class _HomeState extends State<Home> {
           title: const Text('REST API - Mateus'),
         ),
         backgroundColor: Colors.purple,
-        body: () {
-          if (store.dataState == DataState.loading) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          } else if (store.dataState == DataState.loaded) {
-            return ListView.builder(
-              itemCount: store.userModel?.length,
-              itemBuilder: (context, index) {
-                return Card(
-                  margin: const EdgeInsets.all(12.0),
-                  color: Colors.yellow,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(12.0),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(12.0),
-                    child: Column(
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        body: ListenableBuilder(
+          listenable: store,
+          builder: (context, child) {
+            return switch (store.dataState) {
+              DataState.loading => const Center(
+                  child: CircularProgressIndicator(),
+                ),
+              DataState.loaded => ListView.builder(
+                  itemCount: store.userModel?.length,
+                  itemBuilder: (context, index) {
+                    return Card(
+                      margin: const EdgeInsets.all(12.0),
+                      color: Colors.yellow,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12.0),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(12.0),
+                        child: Column(
                           children: [
-                            Text(
-                              store.userModel![index].id.toString(),
-                              style: const TextStyle(
-                                fontSize: 24,
-                                color: Colors.black,
-                              ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                Text(
+                                  store.userModel![index].id.toString(),
+                                  style: const TextStyle(
+                                    fontSize: 24,
+                                    color: Colors.black,
+                                  ),
+                                ),
+                                Text(
+                                  store.userModel![index].name,
+                                  style: const TextStyle(
+                                    fontSize: 24,
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ],
                             ),
-                            Text(
-                              store.userModel![index].name,
-                              style: const TextStyle(
-                                fontSize: 24,
-                                color: Colors.black,
-                                fontWeight: FontWeight.bold,
-                              ),
+                            const SizedBox(
+                              height: 20.0,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                Text(
+                                  store.userModel![index].email,
+                                  style: const TextStyle(
+                                    fontSize: 18,
+                                    //color: Colors.black,
+                                  ),
+                                ),
+                                Text(
+                                  store.userModel![index].website,
+                                  style: const TextStyle(
+                                    fontSize: 18,
+                                    color: Colors.black,
+                                  ),
+                                ),
+                              ],
                             ),
                           ],
                         ),
-                        const SizedBox(
-                          height: 20.0,
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            Text(
-                              store.userModel![index].email,
-                              style: const TextStyle(
-                                fontSize: 18,
-                                //color: Colors.black,
-                              ),
-                            ),
-                            Text(
-                              store.userModel![index].website,
-                              style: const TextStyle(
-                                fontSize: 18,
-                                color: Colors.black,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                );
-              },
-            );
-          } else if (store.error != null) {
-            return Center(
-              child: Text(store.error.toString()),
-            );
-          }
-        }(),
+                      ),
+                    );
+                  },
+                ),
+              DataState.error => const Center(
+                  child: Text('Ocorreu um erro ao buscar os dados'),
+                ),
+            };
+          },
+        ),
       ),
     );
   }
--- lib/user_store.dart
+++ lib/user_store.dart
@@ -5,25 +5,16 @@ import 'package:flutter_projeto_dois/services/api_service.dart';
 class UserStore extends ChangeNotifier {
   late List<UserModel>? userModel = [];
   DataState dataState = DataState.loading;
-  // var loading = false;
-  // var loaded = false;
-  Object? error;
 
   void getData() async {
     dataState = DataState.loading;
-    // loading = true;
-    // loaded = false;
-    error = null;
     notifyListeners();
     try {
       userModel = (await ApiService().getUsers())!;
       dataState = DataState.loaded;
-      // loading = false;
-      // loaded = true;
       notifyListeners();
     } catch (e) {
-      error = e.toString();
-      // loaded = false;
+      dataState = DataState.error;
       notifyListeners();
     }
   }
